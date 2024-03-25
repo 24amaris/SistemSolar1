21c8db9b287ca815d90946ad3b20afdadb3c6f6d
amarispurice <amaris.purice@emmanuel.ro>
2024-03-25 19:36:36 +0200

    am adaugat soarele

--- script.js
+++ script.js
@@ -13,15 +13,22 @@ function setup() {
         ellipse(560, 350, 500, 450);
         ellipse(550, 350, 390, 440);
 
-     //planetele
-        fill("green")
-        circle(1100, 500, 100)
-        fill("lightblue")
-        circle(990, 245, 70)
-        fill("red")
-        circle(270, 235, 50)
-        fill("orange")
-        circle(330, 270, 35)
-        fill("purple")
-        circle(405, 205, 25)
+    //planetele
+        fill("green");
+        circle(1100, 500, 100);
+        fill("lightblue");
+        circle(990, 245, 70);
+        fill("red");
+        circle(270, 235, 50);
+        fill("orange");
+        circle(330, 270, 35);
+        fill("purple");
+        circle(405, 205, 25);
+
+    //soarele
+         fill("yellow");
+         circle(550, 350, 150);
+
+    
+ 
     }
