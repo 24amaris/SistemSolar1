e8ff354bcb600e8a5cadbebaa6c52a145febb1a3
amarispurice <amaris.purice@emmanuel.ro>
2024-03-25 19:08:10 +0200

    am adaugat orbitele planetelor

--- script.js
+++ script.js
@@ -5,4 +5,13 @@ function setup() {
     function draw() {
     background("gray");
 
+    //cercurile linii
+    fill("gray");
+    ellipse(630, 350, 1100, 600);
+    ellipse(600, 350, 850, 560);
+    ellipse(580, 350, 700, 480);
+    ellipse(560, 350, 500, 450);
+    ellipse(550, 350, 390, 440);
+
+
     }
